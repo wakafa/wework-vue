3e503fde329044e49325fec9cbc49c8813a4f141
Mor Cohen <mor.cohen1989@gmail.com>
2020-02-03 16:12:01 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app">
+    <button class="btn btn-hide-show" v-on:click="toggleSeen">{{ button.text }}</button>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos";
+
+export default {
+  name: "app",
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "First todo",
+          content: "Content First",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Second todo",
+          content: "Content Second",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Third todo",
+          content: "Content Third",
+          completed: false
+        },
+        {
+          id: 4,
+          title: "Pick up grandma",
+          content: "from the airport",
+          completed: true
+        },
+        {
+          id: 5,
+          title: "Go for a walk",
+          content: "at least 5km",
+          completed: false
+        }
+      ],
+      button: {
+        text: "Hide my todos"
+      },
+      seen: true
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    toggleSeen() {
+      this.seen = !this.seen;
+      this.button.text = this.seen ? "Hide my todos" : "Show my todos";
+      let display = this.seen ? "block" : "none";
+      document.getElementsByClassName("todo-item").forEach(elem => {
+        elem.style.display = display;
+      });
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.btn-hide-show {
+  margin: 100px;
+  background-color: gray;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p id="todo-item-title">
+      <button class="completed-switch" v-show="!todo.completed" @click="completedSwitch">&#9989;</button>
+      <button class="completed-switch" v-show="todo.completed" @click="completedSwitch">&#10062;</button>
+      {{todo.title}}
+      <button
+        class="del-button"
+        v-show="todo.completed"
+        @click="$emit('del-todo', todo.id)"
+      >&#10060;</button>
+    </p>
+    <p id="todo-item-content">{{todo.content}}</p>
+    <div class="edit" v-show="!todo.completed">
+    <input v-model="todo.content" placeholder="todo content" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    completedSwitch() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+}
+
+#todo-item-title {
+  background-color: rgb(226, 189, 189);
+  font-size: 150%;
+}
+
+#todo-item-content {
+  background-color: rgb(192, 143, 38);
+  font-family: "Courier New", Courier, monospace;
+}
+.is-complete {
+  text-decoration: line-through;
+}
+
+.del-button {
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>
